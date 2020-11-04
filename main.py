c7ec3f4785c6fe6a302eda0fdfeace879f85a7ff
Ian Morrill <ianmorrill@icloud.com>
2020-11-04 13:51:23 -0500

    Set up welcome page and fix static file references

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/test_form")
-def begin_page():
-  return render_template("form.html", title="Hello World!")
+def test_form():
+  return render_template("form.html", title="Hello World!", content="<h1>Hello World</h1>", form_id="0")
+
+@app.route("/welcome")
+def welcome():
+  return render_template("welcome.html")
 
 if __name__ == "__main__":
   app.run()
