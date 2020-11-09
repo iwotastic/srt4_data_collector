2ede0b9286c708445ea31158231624df1be03240
Ian Morrill <ianmorrill@icloud.com>
2020-11-09 11:22:07 -0500

    Add directions page

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def test_form():
   return render_template("form.html", title="Hello World!", content="<h1>Hello World</h1>", form_id="0")
 
+@app.route("/directions")
+def directions():
+  return render_template("directions.html", name="Ian")
+
 @app.route("/welcome")
 def welcome():
   return render_template("welcome.html")
